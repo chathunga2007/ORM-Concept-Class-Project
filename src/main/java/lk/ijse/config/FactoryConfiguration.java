305c376c7721193828245f1248b7c479efb0f054
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-03-10 10:55:57 +0530

    initial commit

--- src/main/java/lk/ijse/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/config/FactoryConfiguration.java
@@ -1,5 +1,34 @@
 package lk.ijse.config;
 
+import lk.ijse.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 //HibernateUtil
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        System.out.println(configuration.getProperties());
+
+        configuration.addAnnotatedClass(Customer.class);
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        return factoryConfiguration == null ? new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
